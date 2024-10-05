d14dca8adcac61a836d6afdcd7b28aab7aa0470d
Simranjeet <simranjeet981444>
2024-10-05 11:15:28 +0530

    Added third card to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,9 @@ function App() {
     <div className="app">
       <Navbar />
       <div className="content">
-        <Card title="Card Title 1" content="This is the content of the first card." />
-        <Card title="Card Title 2" content="This is the content of the second card." />
+        <Card title="Card Title 1" content="Hi Jaswinder Ji." />
+        <Card title="Card Title 2" content="You are welcome" />
+        <Card title="Card Title 3" content="Hope you are doing well." />  {/* New card */}
         <Button label="Click Me" onClick={handleClick} />
       </div>
       <Footer />
